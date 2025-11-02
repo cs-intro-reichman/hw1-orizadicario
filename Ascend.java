348a7bcd0ff332e39d11fefe4420f44986e43930
ori zadicario <ori.zadicario@post.runi.ac.il>
2025-11-02 15:23:18 +0200

    final clean version for autograding

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,17 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	int lim =Integer.parseInt(args[0]);
+	int a = (int)(Math.random()*(lim));
+	int b = (int)(Math.random()*(lim));
+	int c = (int)(Math.random()*(lim));
+
+	int max =Math.max(Math.max(a,b),c);
+	int min=Math.min(Math.min(a,b),c);
+	int middle =(a+b+c)-max-min;
+
+	System.out.println(a+ " "+b+" "+c);
+	System.out.println(min+" "+middle+" "+max);
+
 	}
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,24 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		String period="";
+        if(hours<12){
+			period="AM";
+		}else{
+			hours=hours-12;
+			period="PM";
+		}
+		if(hours==0){
+			hours=0;
+		}else if(hours>12){
+			hours=hours-12;
+		}
+		String minuteString="";
+		if(minutes<10){
+			minuteString="0"+minutes;
+		}else{
+			minuteString=""+minutes;
+		}
+		System.out.println(hours+":"+minuteString+" "+period);
 	}
 }
